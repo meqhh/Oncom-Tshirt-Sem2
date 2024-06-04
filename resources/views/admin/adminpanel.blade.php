69b9f1c7e439d8bc33e7fab8a9ba1738af0567bc
Meqhh <fazrimyazid@gmail.com>
2024-06-04 13:18:41 +0700

    June Commit

---
+++ resources/views/admin/adminpanel.blade.php
@@ -0,0 +1,8 @@
+<section class="bg-[#495867] w-3/4 h-full min-h-screen flex flex-col">
+    <div class="w-full justify-center my-10">
+        <h1 class="text-[#F7F7FF] text-center font-bold text-3xl">Dashboard</h1>
+    </div>
+    <div class="">
+        
+    </div>
+</section>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Oncom</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-[#BDD5EA]">
