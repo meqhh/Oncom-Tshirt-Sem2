9f156f4b8f6b6eb0f358f0c00264e2fe32edb892
Meqhh <fazrimyazid@gmail.com>
2024-06-13 16:13:12 +0700

    Update view listusers

--- resources/views/admin/list_users.blade.php
+++ resources/views/admin/list_users.blade.php
@@ -1,75 +1,50 @@
-<style>
-    table {
-    border: black 1px solid;
-    border-collapse: collapse;
-    width: 80%;
-    margin: 120px auto;
-}
+@extends('layout.main')
+@include('partial.navbar')
 
-th, td {
-    border: black 1px solid; 
-    padding: 8px; 
-    text-align: left; 
-}
-
-th {
-    background-color: #f2f2f2;
-}
-
-tr:nth-child(even) {
-    background-color: #f2f2f2;
-}
-
-tr:nth-child(odd) {
-    background-color: #ffffff;
-}
-.btn-delete {
-        background-color: red;
-        color: white;
-        border: none;
-        padding: 10px 20px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 4px
-}
-</style>
-<div>
-@csrf
-    <table>
-        <theat>
-            <tr>
-                <th>No</th>
-                <th>Nama Users</th>
-                <th>Alamat Users</th>
-                <th>Tanggal Lahir</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Aksi</th>
-            </tr>
-        </theat>
-        <tbody>
-        @foreach ($users as $show )
-        <tr>
-                    <td>{{ $show->user_id }}</td>
-                    <td>{{ $show->name }}</td>
-                    <td>{{ $show->address }}</td>
-                    <td>{{ $show->date_of_birth }}</td>
-                    <td>{{ $show->email }}</td>
-                    <td>{{ $show->password }}</td>
-                    <td> 
+<div class="w-full flex flex-row">
+    <div class="w-1/4">
+        @include('components.adminpanel')
+    </div>
+    <div class="w-3/4 flex content-center flex-col items-center py-20">
+        <div class="mb-4 w-3/4">
+            <h1 class="font-bold text-white text-2xl">
+                List User
+            </h1>
+        </div>
+        @csrf
+        <table class="w-3/4">
+            <thead class="bg-white border-b-2 border-gray-300">
+                <tr lass="w-6">
+                    <th class="px-6 py-2">No</th>
+                    <th class="px-6 py-2">Nama User</th>
+                    <th class="px-6 py-2">Alamat User</th>
+                    <th class="px-6 py-2">Tanggal Lahir</th>
+                    <th class="px-6 py-2">Email</th>
+                    <th class="px-6 py-2">Password</th>
+                    <th class="px-6 py-2">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="bg-white">
+            @foreach ($users as $show )
+                <tr class="bg-gray-100">
+                    <td class="px-4 py-2 text-center">{{ $show->user_id }}</td>
+                    <td class="px-10 py-2 text-center">{{ Str::limit($show->name, 12) }}</td>
+                    <td class="px-4 py-2 text-center">{{ $show->address }}</td>
+                    <td class="px-4 py-2 text-center">{{ $show->date_of_birth }}</td>
+                    <td class="px-4 py-2 text-center">{{ $show->email }}</td>
+                    <td class="px-4 py-2 text-center">{{ Str::limit($show->password, 50) }}</td>
+                    <td class="px-4 py-2 text-center"> 
                         <form action="{{ route('users.destroy', $show->user_id) }}" method="POST" style="display:inline;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn-delete" onclick="return confirm('Are you sure want to delete this?')">Delete</button>
+                            <button type="submit" class="p-4 font-bold text-[#FE5F55] underline" onclick="return confirm('Are you sure want to delete this?')">Delete</button>
                         </form>
                     </td>
                 </tr>
-    @endforeach
-    </tbody>
-    </table>
-</form>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
+
+@include('partial.footer')
